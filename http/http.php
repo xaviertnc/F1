6cd0ac47433ed5bd9a1ec7eab90b3f598f3b87fb
Xavier <xavier.tnc@gmail.com>
2022-06-24 15:21:24 +0200

    F1 PHP Lib v0.7 - NEW http/http.php: http->get() + http->req->{..}

---
+++ http/http.php
@@ -0,0 +1,46 @@
+<?php namespace F1;
+
+/**
+ * 
+ * F1 - HTTP Class
+ * 
+ * @author  C. Moller <xavier.tnc@gmail.com>
+ * 
+ * @version 1.0.0 - 24 Jun 2022
+ *
+ */
+
+class HTTP {
+
+  public $baseUri;
+  public $req;
+
+
+  public function __construct( $baseUri )
+  {
+    $this->baseUri = $baseUri;
+    $req = new \stdClass();
+    $req->uri = $_SERVER[ 'REQUEST_URI' ];
+    $req->path = $this->getRequestPath();
+    $req->segments = $req->path ? explode( '/', $req->path ) : [];
+    $req->method = $_SERVER[ 'REQUEST_METHOD' ];
+    $req->data = $_REQUEST;
+    $this->req = $req;
+  }
+
+
+  public function getRequestPath()
+  {
+    $path = $this->baseUri
+      ? str_replace( $this->baseUri, '', $this->req->uri )
+      : $this->req->uri; 
+    return trim( $path , '/' );
+  }
+
+
+  public function get( $param, $default = null )
+  {
+    return isset( $_REQUEST[ $param ] ) ? $_REQUEST[ $param ] : $default;
+  }
+
+}
